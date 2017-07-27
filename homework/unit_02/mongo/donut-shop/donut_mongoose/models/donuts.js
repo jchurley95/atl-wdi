fd09078c84bb94c254cfbb81c7eb315094ab818f
jchurley95 <jchurley95@gmail.com>
2017-07-26 22:59:23 -0400

    created donut schema

--- homework/unit_02/mongo/donut-shop/donut_mongoose/models/donuts.js
+++ homework/unit_02/mongo/donut-shop/donut_mongoose/models/donuts.js
@@ -1,7 +1,18 @@
+
 //requirements: require mongoose
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+// //create your donut schema:
+var newDonut = new Schema({
+    name: String,
+    description: String,
+    img: String,
+    price: Number,
+    qty: Number,
+});
+var DonutModel = mongoose.model('Donut', newDonut);
 
-//create your donut schema:
+// //export your donut with module.exports()
+module.exports = DonutModel;
 
-
-//export your donut with module.exports()
