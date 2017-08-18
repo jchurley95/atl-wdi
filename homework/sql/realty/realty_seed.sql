92c73d83e9e5a52bb7ff4344bad6e23859fce276
jchurley95 <jchurley95@gmail.com>
2017-08-18 02:53:18 -0400

    Commit: REALTY = Realty seed file created and db seeded

--- homework/sql/realty/realty_seed.sql
+++ homework/sql/realty/realty_seed.sql
@@ -1,1 +1,8 @@
-INSERT INTO players (name, position, salary, team_id) VALUES ('Jameson Konz', 'WR', 390000, 32);
+INSERT INTO Apartments (apartment_number, number_bedrooms, number_bathrooms, address, tenant, occupied, square_footage, price) VALUES (112, 2, 2, '320 Ellen Way', 'Jeff Bridges', true, 240, 1200.00);
+INSERT INTO Apartments (apartment_number, number_bedrooms, number_bathrooms, address, tenant, occupied, square_footage, price) VALUES (203, 5, 3, '403 Huntwick Ct', 'James Jimenez', true, 400, 3000.00);
+
+INSERT INTO Offices (office_number, number_floors, square_footage, number_cubicles, number_bathrooms, address, company_name, occupied, price) VALUES (678, 4, 2000, 30, 8, '894 Courtland Ave', 'SolTech', true, 30000.00);
+INSERT INTO Offices (office_number, number_floors, square_footage, number_cubicles, number_bathrooms, address, company_name, occupied, price) VALUES (511, 5, 1200, 41, 5, '630 Plantation St', 'Generals Assembled', true, 6000.00);
+
+INSERT INTO Storefronts (address, occupied, price, kitchen, square_footage, owner, outdoor_seating) VALUES ('456 Highway 41', true, 4000.00, false, 34253, 'Steve Smith');
+INSERT INTO Storefronts (address, occupied, price, kitchen, square_footage, owner, outdoor_seating) VALUES ('5231 Northview Ave', true, 23451.00, true, 23135135, 'Sarah Roegge');
