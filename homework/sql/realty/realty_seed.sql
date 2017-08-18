a6aec8f561a1bc16daacac28cf577aae8821f518
jchurley95 <jchurley95@gmail.com>
2017-08-18 02:54:48 -0400

    Fixed seeds

--- homework/sql/realty/realty_seed.sql
+++ homework/sql/realty/realty_seed.sql
@@ -4,5 +4,5 @@ INSERT INTO Apartments (apartment_number, number_bedrooms, number_bathrooms, add
 INSERT INTO Offices (office_number, number_floors, square_footage, number_cubicles, number_bathrooms, address, company_name, occupied, price) VALUES (678, 4, 2000, 30, 8, '894 Courtland Ave', 'SolTech', true, 30000.00);
 INSERT INTO Offices (office_number, number_floors, square_footage, number_cubicles, number_bathrooms, address, company_name, occupied, price) VALUES (511, 5, 1200, 41, 5, '630 Plantation St', 'Generals Assembled', true, 6000.00);
 
-INSERT INTO Storefronts (address, occupied, price, kitchen, square_footage, owner, outdoor_seating) VALUES ('456 Highway 41', true, 4000.00, false, 34253, 'Steve Smith');
-INSERT INTO Storefronts (address, occupied, price, kitchen, square_footage, owner, outdoor_seating) VALUES ('5231 Northview Ave', true, 23451.00, true, 23135135, 'Sarah Roegge');
+INSERT INTO Storefronts (address, occupied, price, kitchen, square_footage, owner, outdoor_seating) VALUES ('456 Highway 41', true, 4000.00, false, 34253, 'Steve Smith', true);
+INSERT INTO Storefronts (address, occupied, price, kitchen, square_footage, owner, outdoor_seating) VALUES ('5231 Northview Ave', true, 23451.00, true, 23135135, 'Sarah Roegge', false);
