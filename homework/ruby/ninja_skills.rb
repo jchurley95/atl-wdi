15c6c861c40c6131086b4e447cd4cb8c456d34e5
jchurley95 <jchurley95@gmail.com>
2017-08-21 23:56:01 -0400

    4. String skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -22,3 +22,15 @@ e = Float
 puts e
 d = e
 puts d
+
+# 4
+
+christine = "Hi, I'm Christine"
+likesTo = "long walks on the beach, kinda"
+
+puts christine + " and I like " + likesTo
+
+g = "2"
+gToNumber = 2
+puts g + gToNumber.to_s
+puts g.to_i + gToNumber
