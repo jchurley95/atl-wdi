4b806e884111d3a1db1e30226c25ce8119feec96
jchurley95 <jchurley95@gmail.com>
2017-08-22 00:03:33 -0400

    5. Input/Output skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -34,3 +34,15 @@ g = "2"
 gToNumber = 2
 puts g + gToNumber.to_s
 puts g.to_i + gToNumber
+
+# 5
+
+puts "Enter an integer"
+num = gets
+puts num.to_i * 2
+
+puts "What do they call you, young samurai?"
+name = gets
+puts "Whatcha up to in your free time?"
+whatTheyUpTo = gets
+puts "#{name} does #{whatTheyUpTo}"
